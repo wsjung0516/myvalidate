608b393bfb6731543393073ac3772fc6d2120b1e
wsjung <wsjung@umsun>
2017-07-25 08:55:02 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {SubmittedComponent} from "./submitted.component";
+import {ForbiddenValidatorDirective} from "./forbidden-name.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubmittedComponent,
+    ForbiddenValidatorDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
